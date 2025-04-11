303c49a8eb93b2a20b8e6db7ecb5c56072fabc2c
Henry LETELLIER <henry.letellier@epitech.eu>
2025-04-11 06:13:08 +0200

    [UPDATE] the setup file to be more flexible based on the files present in the repository

--- setup.py
+++ setup.py
@@ -4,17 +4,48 @@ File containing the required information to successfully build a python package
 
 import setuptools
 
+
+def clean_requirements(raw_requirements: list) -> list:
+    """
+    Clean the requirements list
+    """
+    r_index = 0
+    r_length = len(raw_requirements)
+    while r_index < r_length:
+        if raw_requirements[r_index].startswith("#"):
+            print(f"comment removed: {raw_requirements[r_index]}")
+            raw_requirements.pop(r_index)
+            r_length -= 1
+            continue
+        if len(raw_requirements[r_index]) == 0:
+            print(f"empty string removed: '{raw_requirements[r_index]}'")
+            raw_requirements.pop(r_index)
+            r_length -= 1
+            continue
+        if " " in raw_requirements[r_index]:
+            print(f"space removed: {raw_requirements[r_index]}")
+            raw_requirements[
+                r_index
+            ] = raw_requirements[r_index].replace(" ", "")
+        r_index += 1
+    return raw_requirements
+
+
 with open("README.md", "r", encoding="utf-8", newline="\n") as fh:
     long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8", newline="\n") as fh:
+    lib_requirements = fh.read().split("\n")
+
+lib_requirements = clean_requirements(lib_requirements)
+
+print(f"requirements = {lib_requirements}")
 
 setuptools.setup(
     name='window_asset_tkinter',
     version='1.0.0',
     packages=setuptools.find_packages(),
-    install_requires=[
-        "tkcalendar>=1.6.1",
-        "Pillow>=10.4.0"
-    ],
+    install_requires=lib_requirements,
     author="Henry Letellier",
     author_email="henrysoftwarehouse@protonmail.com",
     description="A module that contains boilerplates to speed up the creation of tkinter windows",
